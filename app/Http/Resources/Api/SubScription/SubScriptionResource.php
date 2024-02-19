78f5e2b46c1ac317c94453be5dc8f0f71e09c9b3
mohamedgika <dodm773@gmail.com>
2024-02-19 22:56:22 +0200

    update

--- app/Http/Resources/Api/SubScription/SubScriptionResource.php
+++ app/Http/Resources/Api/SubScription/SubScriptionResource.php
@@ -15,6 +15,7 @@ class SubScriptionResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id'=>$this->id,
             'name'=>$this->name,
             'desc'=>$this->desc,
             'package_details'=>$this->package_details,
