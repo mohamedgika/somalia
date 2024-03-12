b3d1e45db30c2d6924f909d970b4630c80d13f0f
mohamedgika <dodm773@gmail.com>
2024-03-12 22:49:53 +0200

    update

--- app/Http/Controllers/Api/Chat/ChatController.php
+++ app/Http/Controllers/Api/Chat/ChatController.php
@@ -74,7 +74,7 @@ class ChatController extends Controller
 
             $message =  new MassageResource($message);
 
-            broadcast(new SendMessage($request->message))->toOthers();
+            broadcast(new SendMessage($message->message))->toOthers();
             // broadcast(new ChatMessageSent($message))->toOthers();
 
 
